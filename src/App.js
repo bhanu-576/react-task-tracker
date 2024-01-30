59f925c84b84f033cf1f2765a48700c3166d889c
bhanu <bhanusharma576@gmail.com>
2024-01-30 18:39:08 +0530

    project complete

--- src/App.js
+++ src/App.js
@@ -1,67 +1,131 @@
-import {useState} from 'react'
+import {useState,useEffect} from 'react'
+import {BrowserRouter as Router,Route,Routes } from 'react-router-dom'
+
 import Header from './components/Header'
+import Footer from './components/Footer'
+
 import Tasks from './components/Tasks'
 import AddTask from './components/AddTask'
+import About from './components/About'
+
+
 
 
 const App = ()=> {
   const [ showAddTask,setShowAddTask] = useState(false)
 
-  const [tasks,setTasks] = useState(
-    [ 
-  {
-    id: 1,
-    text: 'meeting with the friends',
-    day: "feb 1st at 1:00 pm",
-    reminder : true,
-},
-{
-    id: 2,
-    text: "meeting with the girl friend",
-    day: "feb 1st at 5:00 pm",
-    reminder : true,
-},
-{
-    id: 3,
-    text: "call to home",
-    day: "feb 1st at 9:00 pm",
-    reminder : false,
+  const [tasks,setTasks] = useState([])
+
+//json file establish
+useEffect(()=> {
+  const getTasks = async ()=>{
+    const tasksFromServer = await fetchTasks()
+    setTasks(tasksFromServer)
+  }
+  getTasks()
+},[])
+
+
+//fetch tasks
+const fetchTasks = async ()=>{
+  const res = await fetch('http://localhost:5000/tasks')
+  const data = await res.json() 
+
+  return data
 }
-])
+//fetch task
+const fetchTask = async (id)=>{
+  const res = await fetch(`http://localhost:5000/tasks/${id}`)
+  const data = await res.json() 
+
+  return data
+}
 //Add tak
-const addTask = (task)=>{
-  const id = Math.floor(Math.random() * 10000)+1
-  const newTask = {id, ...task }
-  setTasks([...tasks,newTask])
+const addTask = async (task)=> {
+const res = await fetch('http://localhost:5000/tasks', {
+  method: 'POST',
+  headers : {
+    'Content-type': 'application/json',
+  },
+  body: JSON.stringify(task),
+})
+
+const data = await res.json()
+
+setTasks([...tasks,data])
 }
 
+  //   const id = Math.floor(Math.random() * 10000)+1
+//   const newTask = {id, ...task }
+  // setTasks([...tasks,newTask])
+// }
+
 //Delete Task
-const deleteTask = (id)=> {
-  setTasks(tasks.filter((task) => task.id !== id))
+const deleteTask = async (id) => {
+  const res = await fetch(`http://localhost:5000/tasks/${id}`,{
+    method:'DELETE',
+  })
+
+  // We should control the response tatus to decide if we will change the state or not.
+  res.status === 200
+  ? setTasks(tasks.filter((task) => task.id !== id))
+  :alert('Error Deleting This task')
 }
 
 //Toggle Reminder
-const toggleReminder = (id) => {
+const toggleReminder = async (id) => {
+  const taskToToggle = await fetchTask(id)
+  const updTask  =  {...taskToToggle,reminder: !taskToToggle.reminder} 
+
+  const res = await fetch( `http://localhost:5000/tasks/${id}`, {
+    method: 'PUT',
+    headers : {
+      'Content-type' : 'application/json',
+    },
+    body : JSON.stringify(updTask),
+  })
+
+  const data = await res.json()
+
+
    setTasks(
    tasks.map((task) => 
    task.id === id ? { ...task , reminder:
-    !task.reminder } : task 
+    data.reminder } : task 
     )
   )
 }
   return (
+    <Router>
     <div className='container'>
-      <Header  onAdd = {()=> setShowAddTask
-      (!showAddTask)}
+      <Header  
+      onAdd = {()=> setShowAddTask(!showAddTask)}
         showAdd = {showAddTask}
       />
-      { 
-        showAddTask && <AddTask onAdd = {addTask} />}
-      {
-      tasks.length>0 ? <Tasks  tasks={tasks} onDelete={deleteTask} onToggle={toggleReminder}/>
-      :'No Tasks to show'
-      }</div>
+      <Routes>
+     
+      <Route path='/'
+      element = {
+        <>
+       {showAddTask && <AddTask onAdd = {addTask} />}
+      {tasks.length>0 ? (
+      <Tasks 
+         tasks={tasks} 
+         onDelete={deleteTask} 
+         onToggle={toggleReminder}
+          />
+          ):(
+      'No Tasks to show'
+      )}
+        </>
+      }
+       />
+      <Route path='/about' element={<About />} />
+      </Routes>
+      <Footer />
+      </div>
+      </Router>
   )
 }
 
-export default App;
+export default App
